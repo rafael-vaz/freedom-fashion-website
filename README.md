6b3693aad63028c9579e4d746f33eda4b8cb762e
Rafael Vaz <ferreira.vaz@live.com>
2021-03-11 17:49:23 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Freedom Fashion - Website 
+
+Modelo de site e-commerce de moda desenvolvido para fins de aprendizagem com as linguagem HTML / CSS / JS + JQUERY.
+
---
+++ js/main.js
@@ -0,0 +1,45 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:3
+        },
+
+        400:{
+            items:3
+        },
+
+        600:{
+            items:3
+        },
+
+        
+        700:{
+            items:4
+        },
+
+        
+
+        800:{
+
+            items:4
+        },
+
+        1000:{
+            items:4
+        },
+
+        1020:{
+            items:4
+        },
+
+        1080:{
+            items:5
+        },
+
+        1300:{
+            items:6
+        }
+    }
+})
